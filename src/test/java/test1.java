b48a499d1c87b9fe75dcca94102c9e696654186c
Luchin111 <luisinmedina@gmail.com>
2020-11-09 17:34:33 -0400

    Api testing demo with rest assure

---
+++ src/test/java/test1.java
@@ -0,0 +1,102 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+
+public class test1 {
+
+    //1Probar si la api tiene una respuesta 200 OK
+
+    @Test
+    public void test(){
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println(response.statusCode());
+        System.out.println(response.asString());
+        System.out.println(response.getBody().asString());
+        System.out.println(response.statusLine());
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+
+    }
+
+    //2Probar si la api tiene una respuesta 200 OK y si contiene el id y nombre Michael o Lindsay para mostrar todos los elementos
+    @Test
+    public void test1() {
+
+        given().get("https://reqres.in/api/users?page=2").then().
+                statusCode(200).
+                body("data.id[1]", equalTo(8)).
+                body("data.first_name", hasItems("Michael","Lindsay")).
+                log().all();
+
+    }
+
+
+    //3Post request tenga una respuesta 201 cuando se añada un nuevo recurso
+    @Test
+    public void test2() {
+
+        JSONObject request = new JSONObject();
+        request.put("name", "chaya");
+        request.put("job", "BA");
+
+        System.out.println(request);
+        System.out.println(request.toString());
+
+        given().
+                body(request.toJSONString()).
+                when().
+                post("https://reqres.in/api/users").
+                then().statusCode(201 );
+
+    }
+
+    //4 Put request tenga una respues 200 cuando se reemplaza todas las representaciones actuales del recurso de destino
+    @Test
+    public void test3() {
+
+        JSONObject request = new JSONObject();
+        request.put("name", "chaya");
+        request.put("job", "BAA");
+
+        System.out.println(request);
+        System.out.println(request.toString());
+
+        given().
+                body(request.toJSONString()).
+                when().
+                put("https://reqres.in/api/users/2").
+                then().statusCode(200);
+
+    }
+
+    //5 Patch request tenga una respues 200 cuando se aplica modificaciones parciales a un recurso destino
+    @Test
+    public void test4() {
+
+        JSONObject request = new JSONObject();
+        request.put("name", "chaya");
+        request.put("job", "BAA");
+
+        System.out.println(request);
+        System.out.println(request.toString());
+
+        given().
+                body(request.toJSONString()).
+                when().
+                patch("https://reqres.in/api/users").
+                then().statusCode(200);
+
+    }
+
+
+}
